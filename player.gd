068d4d5659b45f3f1e62f32b55ffe1eec3517885
bas9678 <bryan199663@gmail.com>
2026-01-27 13:59:47 +0100

    movimiento arreglado

--- player.gd
+++ player.gd
@@ -3,49 +3,61 @@ extends CharacterBody2D
 @export var  SPEED = 300.0
 @export var JUMP_VELOCITY = -400.0
 @export var GRAVITY = 1000
+@export var DURACION_ATAQUE = 2
 var muerto: bool
 var ataca: bool = false
+var tiempo_ataque = 1
+var deslizando = false
 
 @onready var animated_sprite = $AnimatedSprite2D
 
 func _physics_process(delta: float) -> void:
+	#SI MUERE NO RECIBE MÁS INPUTS
 	if muerto:
 		return
-	# Gravedad (siempre) para evitar que pj se quede flotando en el aire
+	#gravedad siempre activa para que no flote
 	velocity.y += GRAVITY * delta
-	if !ataca:
-		var input_direccion = Input.get_vector("Izquierda","Derecha","Arriba","Abajo")
-		var horizontal = input_direccion.x
-		var vertical = input_direccion.y
-
-		
-
-		# Salto
-		if is_on_floor() and vertical < 0:
-			velocity.y = JUMP_VELOCITY
-			if not is_on_floor() and vertical < 0 :
-				velocity.y = JUMP_VELOCITY
-		# Movimiento horizontal
-		velocity.x = horizontal * SPEED
-		
-		# Animaciones
-		
-		if not is_on_floor():
-			if animated_sprite.animation != "salto_completo":
-				animated_sprite.play("salto_completo")
-			# Flip en el aire
-			if horizontal != 0:
-				animated_sprite.flip_h = horizontal < 0
-		elif horizontal != 0:
-			if animated_sprite.animation != "Walk":
-				animated_sprite.play("Walk")
-			animated_sprite.flip_h = horizontal < 0
-		else:
-			if animated_sprite.animation != "Idle":
-				animated_sprite.play("Idle")
-	# Mover al personaje
-	move_and_slide()
+	#código para detectar movimiento
+	var input_direccion = Input.get_vector("Izquierda","Derecha","Arriba","Abajo")
+	#IZQUIERDA(-1) DERECHA(+1)
+	var horizontal = input_direccion.x
+	#ARRIBA(-1)-ABAJO(+1)
+	var vertical = input_direccion.y
+	#SI ESTÁ EN EL SUELO Y PULSA ARRIBA -> SALTA
+	if horizontal!=0:
+		animated_sprite.flip_h = horizontal < 0
+	if is_on_floor() and vertical < 0:
+		velocity.y = JUMP_VELOCITY
+	#detectar ataque
+	if Input.is_action_just_pressed("Atacar") and not ataca:
+		ataca = true
+		tiempo_ataque = 1
+		animated_sprite.play("Atack")
+	# Movimiento horizontal
+	velocity.x = horizontal * SPEED
 	
+	#CODIGO PARA ANIMACIONES
+	if ataca:
+		tiempo_ataque += delta
+		if tiempo_ataque >= DURACION_ATAQUE:
+			ataca = false
+	#SI ABAJO (+1) DESLIZAR
+	else:
+		if is_on_floor() and vertical > 0:
+			animated_sprite.play("deslizar")
+		#SI NO ESTA EN EL SUELO SALTO
+		elif not is_on_floor():
+			animated_sprite.play("salto_completo")
+			
+		# SI DERECHA/IZQUIERDA CAMINA
+		elif horizontal !=0 :
+			animated_sprite.play("Walk")
+			
+		#EN EL SUELO Y SIN MOVIMIENTO QUIETO
+		else:
+			animated_sprite.play("Idle")
+	move_and_slide()
+		
 
 
 
