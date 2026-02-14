18e32be456f6510836b63c8f39227d41c04e943f
ivan7931 <ivanivann34@gmail.com>
2026-02-14 03:17:33 +0100

    a

--- meta.gd
+++ meta.gd
@@ -11,11 +11,6 @@ func activar():
 	sprite.visible = true
 	col.disabled = false
 
-func _physics_process(delta):
-	var bodies = get_overlapping_bodies()
-	if bodies.size() > 0:
-		print("Detectando:", bodies)
-
 func _on_body_entered(body: Node2D) -> void:
 	print("Final")
 	if body.is_in_group("player"):
